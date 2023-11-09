f892bf56d42766f2fcab186e4221c60a836cd5ee
Ashley Scarlet <70315656+AshleyScarlet@users.noreply.github.com>
2023-11-09 18:24:56 +0900

    Add property `DisplayName` into `ShaderSettings`

--- API/ShaderSettings.cs
+++ API/ShaderSettings.cs
@@ -5,6 +5,8 @@ namespace gomoru.su.LightController.API
 {
     public abstract partial class ShaderSettings : MonoBehaviour, IEditorOnly
     {
+        public virtual string DisplayName => GetType().Name;
+
         protected virtual void OnEnable() { }
     }
 }
